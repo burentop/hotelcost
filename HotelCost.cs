340eae96ff2e0fbbfbfa18b9551cb8e4caa5f3e7
Brent Perry <contact@brentallan.me>
2019-01-21 17:49:07 -0600

    initialize repo

---
+++ HotelCost.cs
@@ -0,0 +1,12 @@
+using System;
+
+class HotelCost
+{
+    static void Main()
+    {
+        Console.WriteLine("In what season are you booking a stay?");
+        string season = Console.ReadLine();
+        Console.WriteLine("On the weekend or a weeknight?");
+        string dayOfWeek = Console.ReadLine();
+    }
+}
